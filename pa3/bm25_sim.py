4e763740cdddef8ba770b38fe9dbcec471838209
Daniel Rosenberg <drosenbe@Bufonidae.(none)>
2012-05-18 17:28:57 -0700

    Added most of the code. will not run as is.

--- pa3/bm25_sim.py
+++ pa3/bm25_sim.py
@@ -3,22 +3,38 @@ def bm25_sim(q, weights, dictionary, corpus):
     pq = l1_normalize(parse_query(q.query_terms, dictionary, corpus))
     scored_urls = []
     for url in q.query_results:
+        l_f = [0] * 3
+        l_f[2] = url.prepare_anchors(dictionary)[1]
+        l_f[1] = url.prepare_title(dictionary)[1]
+        l_f[0] = url.prepare_body(dictionary)[1]
+    l_f = [float(x)/len(q.query_results) for x in l_f]
+    for url in q.query_results:
         print "For: " + url.title
-        print url.prepare_anchors(dictionary)
-        print url.prepare_title(dictionary)
-        print url.prepare_body(dictionary)
-        print "\n\n\n"
-        anchor_vector = normalized_raw_term_scores(url.prepare_anchors(dictionary), dictionary)
-        body_vector = sublinear_term_scores(url.prepare_body(dictionary), dictionary)
-        title_vector = normalized_raw_term_scores(url.prepare_title(dictionary), dictionary)
-        anchor_vector = l1_normalize(anchor_vector)
-        body_vector = l1_normalize(body_vector)
-        title_vector = l1_normalize(title_vector)
-        anchor_vector = mul(weights[0], anchor_vector)
-        body_vector = mul(weights[1], body_vector)
-        title_vector = mul(weights[2], title_vector)
-        weighted_vector = add(add(anchor_vector, body_vector), title_vector)
-        score = dot(pq, weighted_vector)
+        Bf = 1. # Some arbitrary constant
+        K1 = 1. # another arbitrary constant
+        Wf = [1., 1., 1.] # weights for [Title,Body,Anchor]
+        # normalize weights
+        Wf = [float(x)/dot(Wf,Wf)**.5 for x in Wf]
+        
+        tf = [0]*3
+        l_df = [0] * 3
+        tf[2], l_df[2] = url.prepare_anchors(dictionary)
+        tf[1], l_df[1] = url.prepare_title(dictionary)
+        tf[0], l_df[0] = url.prepare_body(dictionary)
+        # do I need to account for capitalization nonesense?
+        ftf = [dict(),dict(),dict()]
+        w_td = dict()
+        score = 0
+        for term in q.query_terms.split():
+            w_td[term] = 0
+            for f in range(2):
+                if l_f[f] != 0:
+                    ftf[f][term] = tf[f][term]/(1+Bf*(l_df[f][term]/l_f[f]-1))
+                else:
+                    ftf[f][term] = 0
+                w_td[term] += Wf[f]*ftf[f][term]
+        
+        	score += w_td[term]/(K1+w_td[term]) * idf[term] #What is idf?
         scored_urls.append((score, url))
     return scored_urls
 if __name__ == "__main__":
